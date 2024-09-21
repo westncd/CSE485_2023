db3268772d337fbb04b059cb5f37c950075cbc9e
tumngf <buiductung735@gmail.com>
2024-09-22 01:39:17 +0700

    them, sua, xoa the loai

--- admin/category.php
+++ admin/category.php
@@ -1,15 +1,18 @@
+<?php
+include("../database.php");
+$query = "SELECT * FROM theloai";
+$result = mysqli_query($conn, $query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music for Life</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style_login.css">
-
-
+    <title>Music for Life - Categories</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 </head>
 <body>
     <header>
@@ -18,77 +21,67 @@
                 <div class="h3">
                     <a class="navbar-brand" href="#">Administration</a>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Trang ngoài</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="author.php">Tác giả</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
-                    </li>
-                </ul>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="./">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../index.php">Website</a></li>
+                        <li class="nav-item"><a class="nav-link active fw-bold" href="category.php">Categories</a></li>
+                    </ul>
                 </div>
             </div>
         </nav>
-
     </header>
+
     <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
         <div class="row">
             <div class="col-sm">
-                <a href="add_category.php" class="btn btn-success">Thêm mới</a>
-                <table class="table">
+                <h3 class="text-center text-uppercase fw-bold">Categories</h3>
+                <a href="add_category.php" class="btn btn-success mb-3">Thêm thể loại</a>
+                <table class="table table-bordered">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Tên thể loại</th>
-                            <th>Sửa</th>
-                            <th>Xóa</th>
+                            <th scope="col">Sửa</th>
+                            <th scope="col">Xóa</th>
                         </tr>
                     </thead>
                     <tbody>
-                    <form action="" method="GET">
                         <?php
-                            include("../database.php");
-                            if (isset($_GET['this_id'])) {
-                                $this_id = $_GET['this_id'];
-                                $sql = "DELETE FROM theloai WHERE ma_tloai = '$this_id'";
-                                if (mysqli_query($conn, $sql)) {
-                                    // Redirect after deletion
-                                    header("Location: http://localhost/btth01/admin/category.php");
-                                    exit();
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)){
+                                echo '<tr><th scope="row">'.$row["ma_tloai"].'</th><td>'.$row["ten_tloai"].'</td><td><a href="edit_category.php?id=' . $row["ma_tloai"] . '"><i class="fa-solid fa-pen-to-square"></i></a></td><td><a href="category.php?id='.$row['ma_tloai'].'" ><i class="fa-solid fa-trash"></i></a></td></tr>';
+
+                                if (isset($_GET['this_id'])) {
+                                    $this_id = $_GET['this_id'];
+                                    $sql = "DELETE FROM theloai WHERE ma_tloai = '$this_id'";
+                                    if (mysqli_query($conn, $sql)) {
+                                        // Redirect after deletion
+                                        header("Location: http://localhost/btth01/admin/category.php");
+                                        exit();
+                                    }
                                 }
                             }
-                            $query = "SELECT * FROM theloai;";
-                                $result = mysqli_query($conn, $query);
+                        } else {
+                            echo "<tr><td colspan='4'>No categories found</td></tr>";
+                        }
                         
-                                if ($result->num_rows > 0) {
-                                    while ($row = $result->fetch_assoc()) {
-                                        echo '<tr><th scope="row">'.$row["ma_tloai"].'</th><td>'.$row["ten_tloai"].'</td><td><a href="edit_category.php?id=1"><i class="fa-solid fa-pen-to-square"></i></a></td><td><a href="category.php?this_id='.$row['ma_tloai'].'" ><i class="fa-solid fa-trash"></i></a></td></tr>';
-                                    }            
-                                }
                         ?>
-                    </form>
                     </tbody>
                 </table>
             </div>
         </div>
     </main>
+
     <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<?php
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
--- admin/edit_category.php
+++ admin/edit_category.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <?php
-        // include("./category.php");
-        include("../database.php");
+        include("../database.php"); // Include database connection
     ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,77 +20,88 @@
                     <a class="navbar-brand" href="#">Administration</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Trang ngoài</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="author.php">Tác giả</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
-                    </li>
-                </ul>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Trang ngoài</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="author.php">Tác giả</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="article.php">Bài viết</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
-
     </header>
+    
     <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
         <div class="row">
             <div class="col-sm">
                 <h3 class="text-center text-uppercase fw-bold">Sửa thông tin thể loại</h3>
+                
                 <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $ten_tloai = $_POST['txtCatName'];
-                        $ma_tloai = $_POST['txtCatID'];
-                    }
-
-                    $query = 'SELECT * FROM theloai WHERE ma_tloai = 2';
-                    $result = mysqli_query($conn, $query);
-
-
-                    if ($result->num_rows > 0) {
-                            while($row = $result->fetch_assoc()) {
-                                echo '<form action="process_add_category.php" method="post"><div class="input-group mt-3 mb-3"><span class="input-group-text" id="lblCatId">Mã thể loại</span><input type="text" formmethod="post" class="form-control" name="txtCatId" value="'.$row["ma_tloai"].'"></div><div class="input-group mt-3 mb-3"><span class="input-group-text" id="lblCatName">Tên thể loại</span><input type="text" formmethod="post" class="form-control" name="txtCatName" value = "'.$row["ten_tloai"].'"></div><div class="form-group  float-end "><input type="submit" formmethod="post" value="Lưu lại" class="btn btn-success"><a href="category.php" class="btn btn-warning ">Quay lại</a></div></form>';
-                            }
-
-                    }
+                $ma_tloai = '';
+                $ten_tloai = '';
+                if (isset($_GET['id'])) {
+                    $id = mysqli_real_escape_string($conn, $_GET['id']); // Secure the ID
                     
+                    // Fetch category details based on 'id'
+                    $query = "SELECT * FROM theloai WHERE ma_tloai = '$id'";
+                    $result = mysqli_query($conn, $query);
+                    
+                    if ($result && mysqli_num_rows($result) > 0) {
+                        $row = mysqli_fetch_assoc($result);
+                        $ma_tloai = $row['ma_tloai'];
+                        $ten_tloai = $row['ten_tloai'];
+                    }
+                }
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['txtCatId']) && isset($_POST['txtCatName'])) {
+                    $ma_tloai = mysqli_real_escape_string($conn, $_POST['txtCatId']);
+                    $ten_tloai = mysqli_real_escape_string($conn, $_POST['txtCatName']);
+                    $sql = "UPDATE theloai SET ten_tloai = '$ten_tloai' WHERE ma_tloai = '$ma_tloai'";
+                    if (mysqli_query($conn, $sql)) {
+                        header("Location: category.php");
+                        exit();
+                    } else {
+                        echo "Error updating record: " . mysqli_error($conn);
+                    }
+                }
                 ?>
 
-                <!-- <form action="process_add_category.php" method="post">
-                <div class="input-group mt-3 mb-3">
+                <!-- Display the form with existing data -->
+                <form action="edit_category.php?id=<?php echo $ma_tloai; ?>" method="post">
+                    <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatId">Mã thể loại</span>
-                        <input type="text" formmethod="post" class="form-control" name="txtCatId" readonly value="1">
+                        <input type="text" class="form-control" name="txtCatId" value="<?php echo htmlspecialchars($ma_tloai); ?>" readonly>
                     </div>
-
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatName">Tên thể loại</span>
-                        <input type="text" formmethod="post" class="form-control" name="txtCatName" value = "Nhạc trữ tình">
+                        <input type="text" class="form-control" name="txtCatName" value="<?php echo htmlspecialchars($ten_tloai); ?>" required>
                     </div>
-
-                    <div class="form-group  float-end ">
-                        <input type="submit" formmethod="post" value="Lưu lại" class="btn btn-success">
-                        <a href="category.php" class="btn btn-warning ">Quay lại</a>
+                    <div class="form-group float-end">
+                        <input type="submit" value="Lưu lại" class="btn btn-success">
+                        <a href="category.php" class="btn btn-warning">Quay lại</a>
                     </div>
-                </form> -->
+                </form>
             </div>
         </div>
     </main>
-    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
+    
+    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
