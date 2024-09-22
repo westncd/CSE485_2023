bdecb5d31775b1058454221cd6b459c4a3a66486
tumngf <buiductung735@gmail.com>
2024-09-23 00:15:20 +0700

    them database

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+    $server = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $database = 'btth01_cse485';
+
+    $conn = new mysqli($server, $user, $pass, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
